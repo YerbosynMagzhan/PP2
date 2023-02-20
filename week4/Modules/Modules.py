6cb93c9d001e06d04d140f3733d964d92c357a77
YerbosynMagzhan <122903817+YerbosynMagzhan@users.noreply.github.com>
2023-02-20 17:17:14 +0600

    Add files via upload

---
+++ week4/Modules/Modules.py
@@ -0,0 +1,19 @@
+# import mymodule as mx
+
+# mx.greeting("Mazghan")
+
+# a = mx.person1["age"]
+# print(a)
+
+
+# import platform
+
+# x = platform.system()
+# print(x)
+
+
+# x = dir(platform)
+# print(x)
+
+from mymodule import person1
+print(person1["age"])
